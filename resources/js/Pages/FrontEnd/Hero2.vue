7c7321cd1416b7612457920464825187c065b17d
Hasan YILDIZ <hasanyildiz.97@hotmail.com>
2023-02-04 00:05:02 +0300

    hero2 designed as responsive

--- resources/js/Pages/FrontEnd/Hero2.vue
+++ resources/js/Pages/FrontEnd/Hero2.vue
@@ -3,8 +3,8 @@
 <template>
     <section id="hero">
         <div  class="h-screen w-full flex flex-col lg:flex-row  items-center justify-center z-10">
-            <h1 class="text-white  lg:mt-2 font-lale text-lg lg:text-5xl">{{ $t('worldwide') }}</h1>
-            <a href="#contact" class="lg:ml-6 myButton2">{{$t('contact')}}</a> 
+            <h1 class="text-white lg:mt-2 font-lale text-2xl lg:text-5xl">{{ $t('worldwide') }}</h1>
+            <a href="#contact" class="mt-2 lg:mt-0 lg:ml-6 myButton2">{{$t('contact')}}</a> 
         </div>            
     </section>
 </template>
