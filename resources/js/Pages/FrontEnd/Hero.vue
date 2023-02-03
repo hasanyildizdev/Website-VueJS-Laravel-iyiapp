61b21c9dac8ecb3c36c0e16768d62db7dc412e3d
Hasan YILDIZ <hasanyildiz.97@hotmail.com>
2023-02-03 23:57:27 +0300

    iyiapp uplaod2

--- resources/js/Pages/FrontEnd/Hero.vue
+++ resources/js/Pages/FrontEnd/Hero.vue
@@ -1,7 +1,7 @@
 <script setup>
 </script>
 <template>
-    <section id="hero" class="w-full h-screen">
+    <section id="hero" class="w-full h-screen bg-black">
         <div  class="h-screen w-full flex items-center">
                 <div class=" lg:mt-10 mx-4 lg:ml-10 flex flex-col z-10">
                     <h1 class="  text-purple2 text-3xl lg:text-5xl font-lale animate-[leftToRight_2s]">IYIAPP {{$t('software')}}</h1>
@@ -21,7 +21,6 @@
                 <div  class="w-full lg:w-auto flex justify-end absolute -bottom-36 lg:-bottom-44 lg:right-16 right-0">
                     <img src="/img/spaceship.webp" alt="Spaceship" class="w-44 lg:w-auto object-contain">
                 </div>
-
             </div>            
     </section>
 </template>
---
+++ resources/js/Pages/FrontEnd/Hero2.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+<template>
+    <section id="hero">
+        <div  class="h-screen w-full flex flex-col lg:flex-row  items-center justify-center z-10">
+            <h1 class="text-white  lg:mt-2 font-lale text-lg lg:text-5xl">{{ $t('worldwide') }}</h1>
+            <a href="#contact" class="lg:ml-6 myButton2">{{$t('contact')}}</a> 
+        </div>            
+    </section>
+</template>
